e7425622d6d502067df510702cfb826d68c24eba
Jens <jens@diri.no>
2025-05-05 16:22:52 +0200

    Add displayName field to file schema with default value from name

--- models/file.js
+++ models/file.js
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const fileSchema = new mongoose.Schema({
   name: { type: String, required: true }, // Original file name
+  displayName: { 
+    type: String,
+    default: function () {return this.name} 
+  }, // Display name for the file
   data: { type: Buffer, required: true }, // File data as a Buffer
   contentType: { type: String, required: true }, // MIME type of the file
   uploadedAt: { type: Date, default: Date.now }, // Timestamp
