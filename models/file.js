59a84e2f419f186248db1d5f0327e25e5367ab06
Jens <jens@diri.no>
2025-05-15 19:24:15 +0200

    Remove default function for displayName in file schema

--- models/file.js
+++ models/file.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const fileSchema = new mongoose.Schema({
   name: { type: String, required: true }, // Original file name
-  displayName: { 
-    type: String,
-    default: function () {return this.name} 
-  }, // Display name for the file
+  displayName: { type: String }, // Display name for the file
   data: { type: Buffer, required: true }, // File data as a Buffer
   contentType: { type: String, required: true }, // MIME type of the file
   uploadedAt: { type: Date, default: Date.now }, // Timestamp
